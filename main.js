0186f37d3b036d4236442350650102f79c2be722
Changhyun Yoo <chyoo0428@gmail.com>
2022-04-15 23:25:15 +0000

    in progess

--- main.js
+++ main.js
@@ -10,14 +10,11 @@ document.addEventListener('keydown', function rotate(event) {
   } else if (event.keyCode === 39) {
     $img.className = 'east';
   } else if (event.keyCode === 32) {
-    setInterval(moveToRight, 16);
+    setInterval(start, 16);
   }
 });
-
-function moveToRight() {
+function start() {
   var num = 0;
-  for (var i = 0; i < 100; i++) {
-    num = i;
-    var valueOfStyle = 'left:' + num + '%';
-  } return $img.setAttribute('style', valueOfStyle);
+  num += 10;
+  $img.style.left = num + 'px';
 }
