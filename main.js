12335211c4e403f73878a13f04d1f18496a2bb6b
Changhyun Yoo <chyoo0428@gmail.com>
2022-04-15 22:53:12 +0000

    issue#3 completed

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 var $img = document.querySelector('img');
 
-document.onkeydown = function (event) {
-  switch (event.keyCode) {
-    case 37:
-      $img.className = 'west';
-      break;
-    case 38:
-      $img.className = 'north';
-      break;
-    case 39:
-      $img.className = 'east';
-      break;
-    case 40:
-      $img.className = 'south';
-      break;
+document.addEventListener('keydown', function rotate(event) {
+  if (event.keyCode === 40) {
+    $img.className = 'south';
+  } else if (event.keyCode === 37) {
+    $img.className = 'west';
+  } else if (event.keyCode === 38) {
+    $img.className = 'north';
+  } else if (event.keyCode === 39) {
+    $img.className = 'east';
+  } else if (event.keyCode === 32) {
+    setInterval(moveToRight, 16);
   }
-};
+});
+
+function moveToRight() {
+  var num = 0;
+  for (var i = 0; i < 100; i++) {
+    num = i;
+    var valueOfStyle = 'left:' + num + '%';
+  } return $img.setAttribute('style', valueOfStyle);
+}
