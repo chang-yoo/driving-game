1ec0224962958401410c7c63754be492a5f5f93f
Changhyun Yoo <chyoo0428@gmail.com>
2022-04-17 04:45:04 +0000

    revised issue#3

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var $img = document.querySelector('img');
+var position = 0;
+var x = 1200;
 
 document.addEventListener('keydown', function rotate(event) {
   if (event.keyCode === 40) {
@@ -10,11 +12,16 @@ document.addEventListener('keydown', function rotate(event) {
   } else if (event.keyCode === 39) {
     $img.className = 'east';
   } else if (event.keyCode === 32) {
-    setInterval(start, 16);
+    start();
   }
 });
+
 function start() {
-  var num = 0;
-  num += 10;
-  $img.style.left = num + 'px';
+  if (position < x) {
+    position++;
+    $img.style.left = position + 'px';
+    var id = setInterval(start, 16);
+  } else {
+    clearInterval(id);
+  }
 }
